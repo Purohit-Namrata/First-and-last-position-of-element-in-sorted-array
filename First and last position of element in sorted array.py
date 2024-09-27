d213127a6a25748539d3abc813a59814e30037ee
Purohit-Namrata <namratapurohit78@gmail.com>
2024-09-27 12:14:15 +0530

    Update First and last position of element in sorted array.py

--- First and last position of element in sorted array.py
+++ First and last position of element in sorted array.py
@@ -4,7 +4,7 @@ def first_and_last(arr,target):
             start=i
             while i+1 < len(arr) and arr[i+1] == target:
                 i=i+1
-                return [start,i]
+            return [start,i]
     return [-1,-1]
 
 index=first_and_last([5, 7, 7, 8, 8, 10],8)
